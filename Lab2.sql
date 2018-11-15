e340f24e63af7c6e9cc6f3835abf7e33cdbde0c7
m.weber26 <m.weber26@DEN.coloradotech.edu>
2018-11-14 20:04:49 -0700

    updated

--- Lab2.sql
+++ Lab2.sql
@@ -10,27 +10,39 @@
  Use cia_factbook_db
  go
 -- 1) Select a country that has the smallest GDP in the world?
-SELECT TOP (1) 
-[country],[gdp]
+SELECT TOP 1 Country, min(GDP) as lowgdp
 FROM dbo.Economy
-where GDP < 100 ;
+join dbo.Country on country.code = economy.country
+where GDP is Not Null
+group by Country
+order by lowgdp
+go
 
 -- 2) Find the population growth rate for each city?
 -- hint refer to Citypops table and https://pages.uoregon.edu/rgp/PPPM613/class8a.htm for population growth rate
+select *
+from dbo.Citypops
 
 -- 3) Display the most widely spoken language for each country, using a subquery?
-Select *
+Select country,
+name as language,
+percentage
 From dbo.LANGUAGE
-where (Select name AS SpokenLang
-       from Country);
+where exist (
+go
        
 
 -- 4) Display the most widely spoken language in the continent Europe?
 -- hint refer to Encompasses table
-select Name AS SpokenLang,
-Country, Percentage 
-from dbo.LANGUAGE
-where (select spokenlang
-from encompasses
+select *
+from dbo.encompasses
+go
+
+
 
 -- 5) Display all top 5 Countries that have the largest area of Lakes combined?
+select * 
+from dbo.Lake, dbo.geo_Lake
+
+
+go
